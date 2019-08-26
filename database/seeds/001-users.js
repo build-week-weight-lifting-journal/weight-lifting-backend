c9741f138215cdfced9466e156513510d160b3df
MWeberLambdaweb19 <mack.webb37@gmail.com>
2019-08-26 13:51:14 -0700

    Updated project database to have dummy user data

---
+++ database/seeds/001-users.js
@@ -0,0 +1,19 @@
+const bcrypt = require('bcryptjs');
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        {firstName: 'Mack', lastName: 'Weber', password: bcrypt.hashSync('pass', 10), email: 'mack@wlj.to'},
+        {firstName: 'May', lastName: 'Ng', password: bcrypt.hashSync('pass', 10), email: 'may@wlj.to'},
+        {firstName: 'Charles', lastName: 'Wang', password: bcrypt.hashSync('pass', 10), email: 'charles@wlj.to'},
+        {firstName: "Ya'Kuana", lastName: 'Davis', password: bcrypt.hashSync('pass', 10), email: 'yakuana@wlj.to'},
+        {firstName: 'Dan', lastName: 'Hamraj', password: bcrypt.hashSync('pass', 10), email: 'dan@wlj.to'},
+        {firstName: 'Elijah', lastName: 'Foster', password: bcrypt.hashSync('pass', 10), email: 'elijah@wlj.to'},
+        {firstName: 'Leighton', lastName: 'Fritze', password: bcrypt.hashSync('pass', 10), email: 'leighton@wlj.to'},
+        {firstName: 'Joshua', lastName: 'Shockley', password: bcrypt.hashSync('pass', 10), email: 'joshua@wlj.to'},
+        {firstName: 'Dom', lastName: 'Eccleston', password: bcrypt.hashSync('pass', 10), email: 'dom@wlj.to'}
+      ]);
+    });
+};
